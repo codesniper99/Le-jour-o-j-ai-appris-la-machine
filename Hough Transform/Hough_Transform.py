11f2c15dc8dd6535b83ff20f76889c572d81514b
Akhil <akhilvaid21@gmail.com>
2019-05-31 16:15:23 +0530

    Added TLD algorithm implementation

--- Hough Transform/Hough_Transform.py
+++ Hough Transform/Hough_Transform.py
@@ -6,7 +6,7 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 
 edges = cv2.Canny(gray,50,150,apertureSize = 3)
-minLineLength = 10
+minLineLength = 100
 maxLineGap = 1
 lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
 for r in lines:
